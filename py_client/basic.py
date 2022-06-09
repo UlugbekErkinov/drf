8136b81b4d4a2191ff16e64a7c7b99754ac29844
Ulugbek Erkinov <55135871+UlugbekErkinov@users.noreply.github.com>
2022-06-09 13:41:25 +0500

    done

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,3 +1,15 @@
+from calendar import HTMLCalendar
 import requests
 
-endpoint = "https://www.github.com"
+# endpoint = "https://httpbin.org/status/200"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/api/"
+
+get_response = requests.get(endpoint, json={"query":"Hello world"}) 
+print(get_response.text)
+print(get_response.status_code)
+# HTTP request -> HTML
+# REST API HTTP REQUEST -> JSON
+
+print(get_response.json())
+# print(get_response.status_code)
