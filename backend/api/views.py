9613dd29024cca4eba95c0f162f6e0d1ce34f2a7
Ulugbek Erkinov <55135871+UlugbekErkinov@users.noreply.github.com>
2022-06-13 16:54:28 +0500

    Json Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -10,7 +10,8 @@ from products.serializers import ProductSerializer
 def api_home(request, *args, **kwargs):
     
     serializer = ProductSerializer(data=request.data)
-    if serializer.is_valid():
+    if serializer.is_valid(raise_exception=True):
         # instance = serializer.save()
         print(serializer.data)
         return Response(serializer.data)
+    return Response({"invalid":"not good data"}, status=400)
